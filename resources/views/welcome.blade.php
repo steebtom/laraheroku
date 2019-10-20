c90bec605c02f1caf0845327220d5dca27a7a27c
steebtom <steeb.webdev@gmail.com>
2019-10-20 13:33:33 +1300

    Regular Improvement

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -9,12 +9,12 @@
     <title>Steeb Thomas</title>
 </head>
 <body>
-    <!-- <div class="main"> -->
         <div class="name">Steeb Thomas</div>
         <div class="passion">
             <div class="passion1">Web Developer</div>
             <div class="passion2">Test Engineer</div>
             <div class="passion3">QA Analyst</div>
+            <div class="passion3">Geek</div>
         </div>
         <div class="links">
         <a href='https://github.com/steebtom'><img src='/images/gihub.png' height = "28" width = "28"/></a>
@@ -22,8 +22,10 @@
         <a href='https://www.instagram.com/steebthomas/'><img src='/images/instagram.png' height = "28" width = "28"/></a>
 
         </div>
-    <!-- </div> -->
+        <div class="hidden1">
+            Hi there, in case if you're lurkin around xD !!
+        </div>
+        
     <!-- Image Courtesy: Annie Sprat -->
-    <!-- <a style="background-color:black;color:white;text-decoration:none;padding:4px 6px;font-family:-apple-system, BlinkMacSystemFont, &quot;San Francisco&quot;, &quot;Helvetica Neue&quot;, Helvetica, Ubuntu, Roboto, Noto, &quot;Segoe UI&quot;, Arial, sans-serif;font-size:12px;font-weight:bold;line-height:1.2;display:inline-block;border-radius:3px" href="https://unsplash.com/@anniespratt?utm_medium=referral&amp;utm_campaign=photographer-credit&amp;utm_content=creditBadge" target="_blank" rel="noopener noreferrer" title="Download free do whatever you want high-resolution photos from Annie Spratt"><span style="display:inline-block;padding:2px 3px"><svg xmlns="http://www.w3.org/2000/svg" style="height:12px;width:auto;position:relative;vertical-align:middle;top:-2px;fill:white" viewBox="0 0 32 32"><title>unsplash-logo</title><path d="M10 9V0h12v9H10zm12 5h10v18H0V14h10v9h12v-9z"></path></svg></span><span style="display:inline-block;padding:2px 3px">Annie Spratt</span></a> -->
 </body>
 </html>
